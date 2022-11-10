6c8da5e746d6e5ad99ffab962a5c893c753993b8
delishadalu <73183936+delishadalu@users.noreply.github.com>
2022-11-10 12:52:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ movies_data = pd.read_csv('final.csv')
 app = Flask(__name__)
 
 # extracting important information from dataframe
+all_movies = movies_data[["original_title" ,"poster_link" , "release_date" , "runtime" ,"weighted_rating"]]
 
+liked_movies = []
+not_liked_movies = []
+did_not_watch = []
+
+
+def assign_val():
+    m_data = {
+        "original_title": all_movies.iloc[0,0],
+        "poster_link": all_movies.iloc[0,1],
+        "release_date": all_movies.iloc[0,2] or "N/A",
+        "duration": all_movies.iloc[0,3],
+        "rating":all_movies.iloc[0,4]/2
+    }
+    return m_data
 
 # variables to store data
 
